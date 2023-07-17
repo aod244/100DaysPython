24b12969f70215e211176dc992cf0ce590ee7063
Filip Ostrowski <filip.ostrowski96@gmail.com>
2023-07-17 15:36:39 +0200

    3

--- simple_py_apps/24_day.py
+++ simple_py_apps/24_day.py
@@ -1,9 +1,9 @@
-# with open("my_file.txt") as file:
+# with open("/Users/Filip/Desktop/my_file.txt") as file:
 #     contents = file.read()
 #     print(contents)
 
-with open("my_file.txt", mode="w") as file:
-    file.write("yo!")
-
-with open("new_file.txt", mode="w") as file:
-    file.write("dfsafdafs")
+# with open("my_file.txt", mode="w") as file:
+#     file.write("yo!")
+#
+# with open("new_file.txt", mode="w") as file:
+#     file.write("dfsafdafs")
